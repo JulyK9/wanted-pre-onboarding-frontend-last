8f3519271a14af0b31a737300b46977acb52bbb1
JKKIM <jkesonjk@gmail.com>
2023-04-11 17:39:01 +0900

    todo 페이지 토큰 여부에 따른 리다이렉트 로직 추가

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const TodoList = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      navigate('/signin');
+    }
+  });
+
   return <div>TodoList</div>;
 };
 
