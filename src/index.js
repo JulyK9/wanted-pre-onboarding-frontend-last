bbcb87ce8ef8d571056bc4f413e4a8c21054f8d3
JKKIM <jkesonjk@gmail.com>
2023-04-14 18:15:28 +0900

    글로벌 스타일 설정 및 적용

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './style/globalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
